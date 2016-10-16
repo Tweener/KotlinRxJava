6fcdcf018745bbbbc68c13d4bbf2a698726631d1
Vivien Mahe <vivien.mahe@sage.com>
2016-10-16 16:42:50 +0200

    Initial commit

---
+++ app/src/main/java/com/tweener/kotlinrxjava/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tweener.kotlinrxjava
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
