03c7b0f0a0e4c44eff445120d08b235fe4fa6672
Vivien Mahe <vivien.mahe@sage.com>
2016-10-16 19:08:11 +0200

    Skip Realm generated fields when parsing with Gson.

--- app/src/main/java/com/tweener/kotlinrxjava/api/GithubService.kt
+++ app/src/main/java/com/tweener/kotlinrxjava/api/GithubService.kt
@@ -1,7 +1,10 @@
 package com.tweener.kotlinrxjava.api
 
-import com.google.gson.Gson
+import com.google.gson.ExclusionStrategy
+import com.google.gson.FieldAttributes
+import com.google.gson.GsonBuilder
 import com.tweener.kotlinrxjava.model.User
+import io.realm.RealmObject
 import retrofit.GsonConverterFactory
 import retrofit.Retrofit
 import retrofit.RxJavaCallAdapterFactory
@@ -16,7 +19,16 @@ interface GithubService {
 
     companion object {
         fun create(): GithubService {
-            val gson = Gson()
+            // Skip Realm generated fields when parsing
+            val gson = GsonBuilder().setExclusionStrategies(object : ExclusionStrategy {
+                override fun shouldSkipField(f: FieldAttributes): Boolean {
+                    return f.declaringClass == RealmObject::class.java
+                }
+
+                override fun shouldSkipClass(clazz: Class<*>): Boolean {
+                    return false
+                }
+            }).create()
 
             return Retrofit.Builder()
                     .baseUrl("https://api.github.com/")
